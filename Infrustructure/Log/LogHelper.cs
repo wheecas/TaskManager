0749a069cf8dcd62fb0d8a206f2203c9fb1a0260
Allen <280894830@qq.com>
2018-06-03 14:40:16 +0800

    添加日志组件：log4net，简单，易用 详细见 Infrustructure.Log.LogHelper 组件

--- Infrustructure/Log/LogHelper.cs
+++ Infrustructure/Log/LogHelper.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(ConfigFile ="log4net.config",Watch = true)]
 namespace Infrustructure.Log
 {
     /// <summary>
